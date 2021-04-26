a182b4d276624ed6c4b50ae6f3f33402b51d08d3
Konstantinos <59742151+coder12341@users.noreply.github.com>
2021-04-26 15:09:28 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+clear
+
+function install() {
+  if (( $EUID != 0 )); then
+      printf "\nThe installation is for the current user only! If you want to install for all users run as root!\n"
+      echo "The installation will begin in 10 seconds. Press Ctrl+C to abort!"
+      sleep 10
+      echo "Installing..."
+      {
+      cd ~/.local/share/ && mkdir cipher_files && cd cipher_files
+      wget https://github.com/coder12341/cipher-files/releases/download/1.0.0/cipher-files_linux_amd64 -O cipher_files
+      chmod +x qr-generator
+      } &> /dev/null
+      echo 'export PATH=~/.local/share/qr-tools:$PATH' >> ~/.bashrc
+      exit
+  fi
+    
+  echo "The installation will begin in 10 seconds. Press Ctrl+C to abort!"
+  sleep 10
+  echo "Installing..."
+  {
+  cd /bin && wget https://github.com/coder12341/cipher-files/releases/download/1.0.0/cipher-files_linux_amd64 -O cipher_files
+  chmod +x cipher_files
+  
+  } &> /dev/null
+  exit
+}
+
+function uninstall() {
+  echo "Removing..."
+  if (( $EUID != 0 )); then
+    cd ~/.local/share && rm -r cipher_files
+    export PATH=${PATH%:~/.local/share/cipher_files}
+    exit
+  fi
+ cd /bin && rm cipher_files
+ exit
+  
+}
+
+
+printf "(0)Install\n(1)Uninstall\n:"
+read mode
+
+if (( "$mode" == "0" )); then
+  install
+elif (( "$mode" == "1" )); then
+  uninstall
+else
+  echo "Aborting..."
+  exit
+fi
